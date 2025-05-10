ba76e16ef0e67e608accc1c43b1840683a1dcb2c
Geek Neuron <geekneuron@gmail.com>
2025-05-10 15:52:43 +0330

    MainScreen.kt

--- app/src/main/java/com/subpilot/ui/MainScreen.kt
+++ app/src/main/java/com/subpilot/ui/MainScreen.kt
@@ -1,3 +1,18 @@
-package com.subpilot.ui
+package com.geekneuron.subpilot.ui
 
-@Composable fun MainScreen(...) { ... }
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.geekneuron.subpilot.ui.screen.EditorScreen
+
+@Composable
+fun MainScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        EditorScreen()
+    }
+}
