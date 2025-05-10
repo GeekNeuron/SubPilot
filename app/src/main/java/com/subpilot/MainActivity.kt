b57963c0af81f7f518ae5cc7ab24e15af358b9be
Geek Neuron <geekneuron@gmail.com>
2025-05-10 16:01:54 +0330

    MainActivity.kt

--- app/src/main/java/com/subpilot/MainActivity.kt
+++ app/src/main/java/com/subpilot/MainActivity.kt
@@ -1,3 +1,22 @@
-package com.subpilot
+package com.geekneuron.subpilot
 
-class MainActivity : ComponentActivity() { ... }
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.geekneuron.subpilot.ui.theme.SubPilotTheme
+import com.geekneuron.subpilot.ui.navigation.SubPilotNavHost
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SubPilotTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    SubPilotNavHost()
+                }
+            }
+        }
+    }
+}
